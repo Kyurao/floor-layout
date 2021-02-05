c7e67f3548b94b472dfe9134e17b80dab104006c
Kyurao <yuriikutselia@gmail.com>
2021-02-05 20:53:26 +0200

    Add Room, Point

---
+++ src/main/java/com/kyurao/floorlayout/domain/Room.java
@@ -0,0 +1,33 @@
+package com.kyurao.floorlayout.domain;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "ROOM")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ElementCollection
+    @CollectionTable(name="ROOM_CORNERS", joinColumns=@JoinColumn(name="ROOM_ID"))
+    private List<Point> points;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Point> getPoints() {
+        return points;
+    }
+
+    public void setPoints(List<Point> points) {
+        this.points = points;
+    }
+}
