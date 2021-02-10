29c6264d025bd29ff8d689b6104992e83b995344
Kyurao <yuriikutselia@gmail.com>
2021-02-10 14:44:20 +0200

    Add two GET methods with empty pages

--- src/main/java/com/kyurao/floorlayout/controller/RoomController.java
+++ src/main/java/com/kyurao/floorlayout/controller/RoomController.java
@@ -1,7 +1,28 @@
 package com.kyurao.floorlayout.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import static com.kyurao.floorlayout.constant.ViewConstants.TITLE;
+import static com.kyurao.floorlayout.constant.ViewConstants.TOP_MENU_ELEMENT;
 
 @Controller
+@RequestMapping("/room")
 public class RoomController {
+
+    @GetMapping("add")
+    public ModelAndView getViewForAdding() {
+        return new ModelAndView("add-room")
+                .addObject(TITLE, "Create room")
+                .addObject(TOP_MENU_ELEMENT, "createRoom");
+    }
+
+    @GetMapping("all")
+    public ModelAndView allRooms() {
+        return new ModelAndView("all-rooms")
+                .addObject(TITLE, "Rooms")
+                .addObject(TOP_MENU_ELEMENT, "viewRooms");
+    }
 }
