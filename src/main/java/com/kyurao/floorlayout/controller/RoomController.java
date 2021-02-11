6949f0a977c6642c4b46042e810b005d6d6dce90
Kyurao <yuriikutselia@gmail.com>
2021-02-11 18:54:57 +0200

    Move methods for getting views to one controller

--- src/main/java/com/kyurao/floorlayout/controller/RoomController.java
+++ src/main/java/com/kyurao/floorlayout/controller/RoomController.java
@@ -5,17 +5,15 @@ import com.kyurao.floorlayout.domain.Room;
 import com.kyurao.floorlayout.dto.RoomDto;
 import com.kyurao.floorlayout.service.RoomService;
 import com.kyurao.floorlayout.service.ValidatorService;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-import static com.kyurao.floorlayout.constant.ViewConstants.TITLE;
-import static com.kyurao.floorlayout.constant.ViewConstants.TOP_MENU_ELEMENT;
-
-@Controller
-@RequestMapping("/room")
+@RestController
+@RequestMapping("/")
 public class RoomController {
 
     private final ValidatorService validatorService;
@@ -27,29 +25,13 @@ public class RoomController {
         this.roomService = roomService;
     }
 
-    @GetMapping("add")
-    public ModelAndView getViewForAddRoomPage() {
-        return new ModelAndView("add-room")
-                .addObject(TITLE, "Create room")
-                .addObject(TOP_MENU_ELEMENT, "createRoom");
-    }
-
-    @GetMapping("all")
-    public ModelAndView getViewForAllRoomsPage() {
-        return new ModelAndView("all-rooms")
-                .addObject(TITLE, "Rooms")
-                .addObject(TOP_MENU_ELEMENT, "viewRooms");
-    }
-
-    @PostMapping("create")
-    @ResponseBody
+    @PostMapping("room/create")
     public void create(@RequestBody RoomDto req) {
         List<Point> points = validatorService.getValidatedCorners(req.getRoom());
         roomService.saveRoom(new Room(points));
     }
 
     @PostMapping("validateRoom")
-    @ResponseBody
     public RoomDto validateRoom(@RequestBody RoomDto dto) {
         validatorService.getValidatedCorners(dto.getRoom());
         return dto;
--- src/main/java/com/kyurao/floorlayout/controller/ViewController.java
+++ src/main/java/com/kyurao/floorlayout/controller/ViewController.java
@@ -11,12 +11,26 @@ import static com.kyurao.floorlayout.constant.ViewConstants.TOP_MENU_ELEMENT;
 
 @Controller
 @RequestMapping("/")
-public class MainController {
+public class ViewController {
 
     @GetMapping
     public ModelAndView getViewForHomePage() {
         return new ModelAndView("home")
                 .addObject(TITLE, "Main page")
                 .addObject(TOP_MENU_ELEMENT, "home");
+    }
+
+    @GetMapping("room/add")
+    public ModelAndView getViewForAddRoomPage() {
+        return new ModelAndView("add-room")
+                .addObject(TITLE, "Create room")
+                .addObject(TOP_MENU_ELEMENT, "createRoom");
+    }
+
+    @GetMapping("room/all")
+    public ModelAndView getViewForAllRoomsPage() {
+        return new ModelAndView("all-rooms")
+                .addObject(TITLE, "Rooms")
+                .addObject(TOP_MENU_ELEMENT, "viewRooms");
     }
 }
