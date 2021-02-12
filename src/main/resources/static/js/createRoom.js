bff1f9dadbdb3febb58243c0e9eda06b4da94d8b
Kyurao <yuriikutselia@gmail.com>
2021-02-12 15:17:37 +0200

    Add possibility to edit corners on Add room page

--- src/main/resources/static/js/createRoom.js
+++ src/main/resources/static/js/createRoom.js
@@ -7,8 +7,8 @@ function addCorner() {
     const x = selX.val();
     const y = selY.val();
 
-    const rowCnt = $('#cornerTable tbody tr').length;  // get the number of body rows
-    arr[rowCnt] = {x: x, y: y};
+    const index = $('#cornerTable tbody tr').length;  // get the number of body rows
+    arr[index] = {x: x, y: y};
 
     $('#cornerTable tbody')
         .append($('<tr>')
@@ -23,6 +23,12 @@ function addCorner() {
                 )
             )
             .append($('<td>')
+                .append($('<input>')
+                    .attr('type', 'button')
+                    .attr('value', 'Edit')
+                    .attr('class', 'btn btn-outline-secondary w-50')
+                    .attr('onclick', 'editCorner(this)')
+                )
                 .append($('<input>')
                     .attr('type', 'button')
                     .attr('value', 'Remove')
@@ -41,6 +47,26 @@ function removeCorner(oButton) {
     const index = row.index();
     arr.splice(index, 1);
     row.remove();
+}
+
+function editCorner(oButton) {
+    const row = $(oButton).closest('tr');
+    const index = row.index();
+
+    const selEditX = $('#editX');
+    const selEditY = $('#editY');
+    selEditX.val(arr[index].x);
+    selEditY.val(arr[index].y);
+    $('#editModal').modal('show');
+    $('#editSave').click(function () {
+        const x = selEditX.val();
+        const y = selEditY.val();
+        arr[index].x = x;
+        arr[index].y = y;
+        $(row).find("td").eq(0).text(x);
+        $(row).find("td").eq(1).text(y);
+        $('#editModal').modal('hide');
+    });
 }
 
 async function createRoom() {
